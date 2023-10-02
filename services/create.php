0899b6cb3cc322c829ac8a8865ccb54249c527b0
Lukitas <flucasyoel@outlook.es>
2023-10-02 11:23:35 -0300

    Add Security

--- services/create.php
+++ services/create.php
@@ -14,22 +14,42 @@ if ($_POST['submit']) {
     $email = $_POST['email'];
     $curso = $_POST['curso'];
 
-    $query = "INSERT INTO t_alumnos_del_curso(nombreDelUsuario,mail,codigoCurso) VALUES ('$nombre','$email','$curso')";
-    $result = mysqli_query($conexion, $query);
+    $querySelect = "SELECT * FROM t_alumnos_del_curso where mail='$email'";
+    $resultSelect = mysqli_query($conexion, $querySelect);
 
-    if (!$result) {
-        die('Error en la Query Insert ERROR :' . mysqli_error($conexion));
+    $numRows = mysqli_num_rows($resultSelect);
+    if ($numRows > 0) {
+        $_SESSION['message_input_vacio'] = 'El Usuario ya se encuentra dado de alta';
+        header("Location: ../pages/alta.php");
+
+    } else {
+        $queryInsert = "INSERT INTO t_alumnos_del_curso(nombreDelUsuario,mail,codigoCurso) VALUES ('$nombre','$email','$curso')";
+        $resultInsert = mysqli_query($conexion, $queryInsert);
+
+        $_SESSION['message'] = 'Alumno dado de alta';
+
+
+        if (!$resultInsert) {
+            die('Error en la Query Insert ERROR :' . mysqli_error($conexion));
+
+        }
+
+
+
+
+        header("Location: ../pages/alta.php");
+
 
     }
 
 
-    $_SESSION['message'] = 'Alumno dado de alta';
 
 
 
 
 
 
-    header("Location: ../pages/alta.php");
+
+
 }
 ?>
